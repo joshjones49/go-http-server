3b22b867e0c7690cdaa26feb9febb6372a86ff1c
joshjones49 <joshjones200285@gmail.com>
2025-11-05 10:35:09 -0500

    Created basic starting point for http server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+var PORT string = "8080"
+
 func main() {
-	
+	// used to direct requests, think of it as a traffic controller
+	mux := http.NewServeMux()
+
+	// console log to show where sever is running
+	fmt.Println("Server Running On Port: " + PORT)
+	// function that starts server
+	http.ListenAndServe(PORT, mux)
 }
